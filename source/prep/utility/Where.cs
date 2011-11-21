da5e14f3473d5be68196897db2622ff52a5545a5
Stefan Moser <stefanm@iqmetrix.com>
2011-11-21 10:32:39 -0800

    Pushing new changes

---
+++ source/prep/utility/Where.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prep.utility
+{
+    public class Where<Item>
+    {
+        public static void has_a(Func<Item, object> func)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
