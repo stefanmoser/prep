0e62ed3d8b89cc722a57707403d15572135f3638
Stefan Moser <stefanm@iqmetrix.com>
2011-11-22 09:11:47 -0600

    Pushing new changes

---
+++ source/prep.specs/bloom/BloomFilterSpecs.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+
+namespace prep.bloom
+{
+    [Subject(typeof(BloomFilter))]
+    public class BloomFilterSpecs
+    {
+        public abstract class bloom_filter_concern : Observes<BloomFilter>
+        {
+            protected static BloomFilter bloom_filter;
+            protected static List<string> dictionary = new List<string>();
+
+            protected static List<string> words = new List<string>(){"hi", "there", "develop", "with", "passion"};
+            protected static List<string> not_words = new List<string>(){"asdf", "qwer", "ZXC", "hkghkj", "asdf"};
+
+            Establish context = () => bloom_filter = new BloomFilter();
+        }
+
+        public class when_initializing_with_no_words : bloom_filter_concern
+        {
+           
+            private Because of = () => bloom_filter.initialize_with(dictionary);
+
+            private It should_not_match_words = () =>
+                                                    {
+                                                        foreach (var possible_word in words)
+                                                        {
+                                                            bool is_in_dictionary = bloom_filter.check_word_in_dictionary(possible_word);
+                                                            is_in_dictionary.ShouldBeFalse();
+                                                        }
+                                                    };
+        }
+
+        public class when_initializing_with_one_word : bloom_filter_concern
+        {
+            private Because of = () => bloom_filter.initialize_with(new[] {"regina"});
+
+            private It should_match_the_word = () => bloom_filter.check_word_in_dictionary("regina").ShouldBeTrue();
+
+            private It should_not_match_other_words = () =>
+                                                          {
+                                                              foreach (var possible_word in words)
+                                                              {
+                                                                  bloom_filter.check_word_in_dictionary(possible_word).ShouldBeFalse();
+                                                              }
+                                                          };
+            It should_not_match_an_empty_string = () =>
+        }
+
+
+
+//            public abstract class movie_library_concern : Observes<MovieLibrary>
+//    {
+//      protected static IList<Movie> movie_collection;
+//
+//      Establish c = () =>
+//      {
+//        movie_collection = new List<Movie>();
+//        depends.on(movie_collection);
+//      };
+//    };
+//
+//    public class when_iterating : movie_library_concern
+//    { 
+    }
+}
---
+++ source/prep/bloom/BloomFilter.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace prep.bloom
+{
+    public class BloomFilter
+    {
+        private byte[] _array;
+        private const int array_size = 256;
+
+        public BloomFilter()
+        {
+            _array = new byte[array_size];
+        }
+
+        public void initialize_with(IEnumerable<string> dictionary)
+        {
+            foreach (var word in dictionary)
+            {
+                insert_word_into_hash(word);
+            }
+        }
+
+        private void insert_word_into_hash(string word)
+        {
+            var hash = hash_word(word);
+
+            foreach (byte b in hash)
+            {
+                _array[b] = 1;
+            }
+        }
+
+        private static byte[] hash_word(string word)
+        {
+            byte[] bytes_from_word = new byte[word.Length];
+            for (int i = 0; i < word.Length; i++)
+            {
+                bytes_from_word[i] = (byte) word[i];
+            }
+
+            MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(bytes_from_word);
+            return hash;
+        }
+
+        public bool check_word_in_dictionary(string possbile_word)
+        {
+            byte[] word_hash = hash_word(possbile_word);
+
+            foreach (byte b in word_hash)
+            {
+                if (_array[b] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
